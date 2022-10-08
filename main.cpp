622f0177d8a77d52a7e1dcfff1c00c904ab7f6b9
Kyuwoong Lee <kwooler@gmail.com>
2022-10-08 23:41:47 +0000

    Q71

--- main.cpp
+++ main.cpp
@@ -8,21 +8,11 @@ const int N = 10;
 int main()
 {
 	int number[N] = {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
-	int max, idx, i;
 
-	for (i = 0; i < N - 1; i++)
-	{
-		if ((i == 0) || getDiff(number, i) > max)
-		{
-			max = getDiff(number, i);
-			idx = i;
-		}
-	}
-	cout << "The pair is " << number[idx] << "\t" << number[idx + 1] << endl;
+	/* Make your code here */
+	// Find two adjacent values that have the largest gap and Print them.
 }
 
 int getDiff(int n[], int i)
 {
-	int diff = abs(n[i] - n[i + 1]);
-	return diff;
 }
